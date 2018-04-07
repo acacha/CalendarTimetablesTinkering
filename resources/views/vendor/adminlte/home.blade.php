ef2b5ed1ea100d36f2b729f5a29c7f44b6d840d3
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-04-07 15:00:04 +0200

    Initial version

---
+++ resources/views/vendor/adminlte/home.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+	{{ trans('adminlte_lang::message.home') }}
+@endsection
+
+
+@section('main-content')
+	<div class="container-fluid spark-screen">
+		<div class="row">
+			<div class="col-md-12">
+				<tinkering-calendar></tinkering-calendar>
+			</div>
+		</div>
+	</div>
+@endsection
